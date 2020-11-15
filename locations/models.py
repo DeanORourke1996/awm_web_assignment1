b6f9650d14235d8c186e8b34419ce7b40393ef84
Dean O'Rourke <dean@Deans-MacBook-Pro.local>
2020-11-15 21:57:38 +0000

    removed some redundant files/directories and added templates to each app

--- locations/models.py
+++ locations/models.py
@@ -3,12 +3,24 @@ from django.contrib.gis.db import models
 
 
 # Create your models here.
-class Locations(models.Model):
-    name = models.CharField('Name', max_length=90)
-    country = models.CharField('Country', max_length=50)
-    rating = models.DecimalField('Rating', max_digits=2, decimal_places=1)
-    lon = models.FloatField('Longitude')
-    lat = models.FloatField('Latitude')
+class WorldBorder(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.IntegerField('Population 2005')
+    fips = models.CharField('FIPS Code', max_length=2, null=True)
+    iso2 = models.CharField('2 Digit ISO', max_length=2)
+    iso3 = models.CharField('3 Digit ISO', max_length=3)
+    un = models.IntegerField('United Nations Code')
+    region = models.IntegerField('Region Code')
+    subregion = models.IntegerField('Sub-Region Code')
+    lon = models.FloatField()
+    lat = models.FloatField()
 
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    mpoly = models.MultiPolygonField()
+
+    # Returns the string representation of the model.
     def __str__(self):
-        return self.name
+        return self.
